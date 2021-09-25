3691d76cc2975416455892d7d6989a9f0550dfef
apoorvshrimali <apoorv.shrimali@gmail.com>
2021-09-26 01:51:06 +0530

    done

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const billAmount = document.querySelector("#bill-amount");
 const checkButton = document.querySelector("#check-btn");
 const cashGiven = document.querySelector("#cash-given");
 const errorMessage =document.querySelector("#error-message")
+const NumberOfNotes = document.querySelectorAll(".number-of-notes") 
+
+
 checkButton.addEventListener("click", checkEventHandler );
 function checkEventHandler(){
 
@@ -14,6 +17,14 @@ else if (Number(cashGiven.value)<=Number(billAmount.value)){
 console.log("CASH GIVEN IS LESS THAN BILL AMOUNT")
 }
 else {
-    var result ="cashGiven.value-billAmount.value"
+    console.log(NumberOfNotes)
+    const notes = [2000,500, 200, 100 ,50 ,20 ,10 ,5 , 2 , 1 ]
+    var netAmount =cashGiven.value-billAmount.value
+    var prevAmount = netAmount  
+    for(let i = 0 ;i < notes.length ; i++){
+        const result  = Math.floor(prevAmount / notes[i])
+        NumberOfNotes[i].innerText = result // 2
+        prevAmount = prevAmount % notes[i]  
+    }
 }
 }
